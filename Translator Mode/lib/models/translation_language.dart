6c4e3d3ae133e3070aa3af1f5618d54917a41db7
emilalvaroserrano-collab <emilalvaroserrano@gmail.com>
2026-09-10 00:02:26 +0800

    Add Android Speech Recognition locale tags

--- Translator Mode/lib/models/translation_language.dart
+++ Translator Mode/lib/models/translation_language.dart
@@ -5,28 +5,62 @@ class TranslationLanguage {
     required this.code,
     required this.displayName,
     required this.ttsCode,
+    required this.sttCode,
   });
 
   final String code;
   final String displayName;
   final String ttsCode;
+  final String sttCode;
 }
 
 const translationLanguages = <TranslationLanguage>[
-  TranslationLanguage(code: 'en', displayName: 'English (US)', ttsCode: 'en'),
+  TranslationLanguage(
+    code: 'en',
+    displayName: 'English (US)',
+    ttsCode: 'en',
+    sttCode: 'en-US',
+  ),
   TranslationLanguage(
     code: 'tl',
     displayName: 'Tagalog (Filipino)',
     ttsCode: 'tl',
+    sttCode: 'fil-PH',
   ),
   TranslationLanguage(
     code: 'nl-BE',
     displayName: 'Dutch (Flemish)',
     ttsCode: 'nl',
+    sttCode: 'nl-BE',
   ),
-  TranslationLanguage(code: 'fr', displayName: 'French', ttsCode: 'fr'),
-  TranslationLanguage(code: 'de', displayName: 'German', ttsCode: 'de'),
-  TranslationLanguage(code: 'es', displayName: 'Spanish', ttsCode: 'es'),
-  TranslationLanguage(code: 'it', displayName: 'Italian', ttsCode: 'it'),
-  TranslationLanguage(code: 'pt', displayName: 'Portuguese', ttsCode: 'pt'),
+  TranslationLanguage(
+    code: 'fr',
+    displayName: 'French',
+    ttsCode: 'fr',
+    sttCode: 'fr-FR',
+  ),
+  TranslationLanguage(
+    code: 'de',
+    displayName: 'German',
+    ttsCode: 'de',
+    sttCode: 'de-DE',
+  ),
+  TranslationLanguage(
+    code: 'es',
+    displayName: 'Spanish',
+    ttsCode: 'es',
+    sttCode: 'es-ES',
+  ),
+  TranslationLanguage(
+    code: 'it',
+    displayName: 'Italian',
+    ttsCode: 'it',
+    sttCode: 'it-IT',
+  ),
+  TranslationLanguage(
+    code: 'pt',
+    displayName: 'Portuguese',
+    ttsCode: 'pt',
+    sttCode: 'pt-PT',
+  ),
 ];
